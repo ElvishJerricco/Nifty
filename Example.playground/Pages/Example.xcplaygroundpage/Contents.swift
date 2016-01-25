7d24d89f8d85104ee214f513695d373d68858f86
ElvishJerricco <elvishjerricco@gmail.com>
2016-01-24 20:14:35 -0500

    Simple example in the example playground

--- Example.playground/Pages/Example.xcplaygroundpage/Contents.swift
+++ Example.playground/Pages/Example.xcplaygroundpage/Contents.swift
@@ -1,4 +1,9 @@
 //: [Previous](@previous)
 import Nifty
 
+let x = (1..<40).stream().reduce(0, reducer: +)
+let y = (1..<50).stream().reduce(0, reducer: +)
+
+let sum = curry(+) <^> x <*> y
+sum.wait()
 //: [Next](@next)
