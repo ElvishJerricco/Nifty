ff9a4cd72be19c5285a584cd9d4c615ba4e5307d
ElvishJerricco <elvishjerricco@gmail.com>
2016-01-25 02:33:56 -0500

    Added an append and prepend + operator for arrays

--- Sources/Operators.swift
+++ Sources/Operators.swift
@@ -22,3 +22,17 @@ infix operator >>== { associativity left }
 func *<A, B, C>(g: B -> C, f: A -> B) -> A -> C {
     return { a in g(f(a)) }
 }
+
+// Append element to array
+
+func +<T>(element: T, array: [T]) -> [T] {
+    var array = array
+    array.insert(element, atIndex: 0)
+    return array
+}
+
+func +<T>(array: [T], element: T) -> [T] {
+    var array = array
+    array.append(element)
+    return array
+}
