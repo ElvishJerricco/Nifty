db5f4a9266bb24ecff104793cfaa240aeff01cab
ElvishJerricco <elvishjerricco@gmail.com>
2016-02-03 20:07:12 -0500

    Fixed stream playground example

--- Example.playground/Pages/Example.xcplaygroundpage/Contents.swift
+++ Example.playground/Pages/Example.xcplaygroundpage/Contents.swift
@@ -1,8 +1,8 @@
 //: [Previous](@previous)
 import Nifty
 
-let x = (1..<40).stream().reduce(0, reducer: +)
-let y = (1..<50).stream().reduce(0, reducer: +)
+let x = (1..<40).stream().reduce(0, combine: +)
+let y = (1..<50).stream().reduce(0, combine: +)
 
 let sum = curry(+) <^> x <*> y
 sum.wait()
